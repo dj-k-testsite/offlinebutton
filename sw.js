7dbc75914efa1f58f167070819d43ba1b0e5a690
dj-k-testsite <38774475+dj-k-testsite@users.noreply.github.com>
2018-04-27 16:16:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener("install", function(e) {
     caches.open("offlinebutton").then(function(cache) {
       return cache.addAll([
         './index.html',
-        '/index.html?homescreen=1',
+        './index.html?homescreen=1',
         './scripts.js',
       ]);
     })
