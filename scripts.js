ed8589ba35fc7c97a1c5e738628b91a1f7d8c9a1
dj-k-testsite <38774475+dj-k-testsite@users.noreply.github.com>
2018-04-27 16:59:53 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
-document.getElementsByTagName('input')[0].disable = true;
-document.getElementsByTagName('input')[0].disable = false;
+let fileInput = document.getElementsByTagName('input')[0];
+alert(fileInput.disabled);
+fileInput.addEventListener('touchstart', function() { alert('tap'); fileInput.click(); });
+
+alert(body.disabled);
+alert(html.disabled);
