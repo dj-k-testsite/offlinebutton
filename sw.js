fdc0e91b3dd9ddcebc82777bae846299cab8a59b
dj-k-testsite <38774475+dj-k-testsite@users.noreply.github.com>
2018-04-27 15:03:42 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("halogen-web-audio-player-demo").then(function(cache) {
+      return cache.addAll([
+        "./index.html",
+        "./script.js",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log(event.request.url);
+  fetch(event.request).catch(function() {
+    return caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        alert("File Missing and cannot fetch while offline");
+        throw new Error("File Missing and cannot fetch while offline");
+      }
+    });
+  });
+});
